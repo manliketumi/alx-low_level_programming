21873c91114cfad8e9ea95636628991d81e9e276
manliketumi <mongwegeloa@gmail.com>
2023-07-17 02:12:20 -0700

    lower case alph and number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - Print lowercase numbers from 0 to 9 and alphabet a to f
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
