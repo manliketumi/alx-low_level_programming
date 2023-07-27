260f5b9213aa09c5f51bc8c8efe9bf9ebc36082c
manliketumi <mongwegeloa@gmail.com>
2023-07-27 03:27:04 -0700

    helloworld

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *_strcat -  function that concatenates two strings
+ * @src: first input string
+ * @dest: second input string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
