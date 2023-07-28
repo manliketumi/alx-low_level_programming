619758882a86242e88aaa893612cf060d2f8d00c
manliketumi <mongwegeloa@gmail.com>
2023-07-28 13:48:42 -0700

    cap hello

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,5 +8,26 @@
 
 char *cap_string(char *str)
 {
+	int i, j;
 
+	char seperators[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == seperators[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (str);
 }
