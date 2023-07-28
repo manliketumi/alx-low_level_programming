f4dc4c8e217cb8f498e46db5da9c48dbc253f20d
manliketumi <mongwegeloa@gmail.com>
2023-07-28 11:48:11 -0700

    solanka

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: string
+ *
+ * Return: string characters
+*/
+
+char *cap_string(char *str)
+{
+
+}
