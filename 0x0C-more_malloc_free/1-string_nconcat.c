d616419ddc89b6778750ec10b1ec7c3133e9164d
manliketumi <mongwegeloa@gmail.com>
2023-08-10 14:53:03 -0700

    will debug it

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,11 +4,26 @@
  * string_nconcat - concentrate on two string
  * @s1: first string
  * @s2: second string
- * @n: 
+ * @n: number of bytes from s1
  * Return: NULL if fail
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *siri = s1;
 
+	while (*siri != '\0')
+	{
+		siri++;
+	}
+	while (*s2 != '\0' && n > 0)
+	{
+		*siri = *s2;
+		siri++;
+		s2++;
+		n--;
+	}
+	*siri = '\0';
+
+	return (s1);
 }
