746bd418967d1376b40ef862a3692bef89d48eac
manliketumi <mongwegeloa@gmail.com>
2023-08-15 01:32:55 -0700

    02-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a file
+ * Description: print a file name that is compiled
+ * Return - always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
