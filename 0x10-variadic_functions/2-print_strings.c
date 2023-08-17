4ea821b3ab3ce65b64b25b3b874dc56715dbcea3
manliketumi <mongwegeloa@gmail.com>
2023-08-17 14:39:59 -0700

    so many thing bruv

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		const char *str = va_arg(args, const char *);
+		str = va_arg(args, const char *);
 
 		if (str == NULL)
 			printf("(nil)");
