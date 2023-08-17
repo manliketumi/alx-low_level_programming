aff1f487bfc7958132d6354606c7ebd2f3d198fc
manliketumi <mongwegeloa@gmail.com>
2023-08-17 15:33:15 -0700

    bar for bar value

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,14 +2,47 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_strings - print a string 
- * @separator: string to be printed between strings
- *
- *
+ * print_all - function that print anything
+ * @format: is a list of types of arguments passed
+ * to the function
+ * Return: void
  */
 
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i;
-	
+	unsigned int i = 0;
+	char *separator = "";
+	const char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
