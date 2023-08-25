94f80a86e2a6679b851cc78e663d3bae47ab1c3a
Tumisang <mongwegeloa@gmail.com>
2023-08-25 02:38:43 -0700

    sqrt figure

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <math.h>
 /**
+ * natural_square_root - to check if its natural
+ * @a: first int type
+ * @b: second int type
+ * Return: success
+ */
+
+int natural_square_root(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+	if (b * b > a)
+		return (-1);
+	return (natural_square_root(a, b + 1));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root
  * of a number
  * @n: string
@@ -9,12 +25,11 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == '\0')
-	{
+	if (n == 1)
+		return (1);
+	if (n == 0)
+		return (0);
+	if (n  < 1)
 		return (-1);
-	}
-	else
-	{
-		return (n * _sqrt_recursion(n + 1));
-	}
+	return (natural_square_root(n, 1));
 }
