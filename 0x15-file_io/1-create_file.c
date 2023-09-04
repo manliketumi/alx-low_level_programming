d8c336b30ae3e54833ffcbcd28522c25988dab12
Tumisang <mongwegeloa@gmail.com>
2023-09-04 15:00:28 -0700

    created file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		write_result = write(file_descriptor ,text_content ,strlen(text_content));
+		write_result = write(file_descriptor, text_content, strlen(text_content));
 		if (write_result == -1)
 		{
 			close(file_descriptor);
