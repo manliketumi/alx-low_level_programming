04dad9feda4d0b2babc1c06c1a1305fc7c55fa6f
manliketumi <mongwegeloa@gmail.com>
2023-10-26 14:25:48 -0700

    debugg

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,9 +8,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= 64)
+	{
 		return (-1);
+	}
 
-	*n |= (1UL << index);
-	return (-1);
+	*n |= 1 << index;
+	return (1);
 }
