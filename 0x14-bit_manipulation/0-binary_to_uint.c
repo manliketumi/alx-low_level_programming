057e31f4bb289e8e379750304bbe102d5b4ca495
manliketumi <mongwegeloa@gmail.com>
2023-10-26 21:02:35 -0700

    debugged

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,25 +10,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
-int power = 0;
+
 if (b == NULL)
-{
 return (0);
-}
 
 while (*b != '\0')
 {
 if (*b != '0' && *b != '1')
-{
 return (0);
-}
 
-if (*b == '1')
-{
-result |= (1 << power);
-}
-
-power++;
+result = (result << 1) | (*b - '0');
 b++;
 }
 
