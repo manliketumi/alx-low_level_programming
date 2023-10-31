e077cb68a608471de46dd7c5a847d1255aa8a1dc
manliketumi <mongwegeloa@gmail.com>
2023-10-31 14:34:08 -0700

    append fild

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	
+
 	struct stat st;
 	{
 	if (filename == NULL)
@@ -31,12 +31,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		ssize_t bytes_written = write(file, text_content, strlen(text_content));
-
-		if ((bytes_written = strlen(text_content)))
-		{
-			close(file);
-			return (-1);
-		}
+	{
+	if ((bytes_written = strlen(text_content)))
+	{
+		close(file);
+		return (-1);
+	}
+	}
 	}
 	close(file);
 	return (1);
