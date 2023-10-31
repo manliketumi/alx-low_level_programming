1dc440435e955d54bbc1973f61fc117a6d651466
manliketumi <mongwegeloa@gmail.com>
2023-10-31 14:38:09 -0700

    trying

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,35 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
+	int fh, i = 0, j = 0;
 
-	struct stat st;
-	{
 	if (filename == NULL)
 		return (-1);
-	}
-	if (stat(filename, &st) == 0)
-	{
-		if (truncate(filename, 0) != 0)
-			return (-1);
-	}
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	{
-		if (file == -1)
-			return (-1);
-	}
+
+	fh = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fh == -1)
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		ssize_t bytes_written = write(file, text_content, strlen(text_content));
-	{
-	if ((bytes_written = strlen(text_content)))
-	{
-		close(file);
-		return (-1);
+		while (text_content[i])
+			i++;
+		j = write(fh, text_content, i);
+		if (i != j)
+			return (-1);
 	}
-	}
-	}
-	close(file);
+	close(fh);
 	return (1);
 }
