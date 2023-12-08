d9101c70d7daa8d77e7fd0239e949ec66fb5bbda
manliketumi <mongwegeloa@gmail.com>
2023-12-07 21:31:30 -0800

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,14 +32,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	for (i = 0; current != NULL && i < idx - 1; i++)
-	{
 		current = current->next;
-	}
 
 	if (current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+	new_node->next = current->next;
+	new_node->prev = current;
+
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 	return (new_node);
 }
